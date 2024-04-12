4afcf84bb097fc9dc281f4da49a51fa2cc8fbc9e
WAFFEN1998 <davidmichalica98@email.cz>
2024-04-12 12:13:36 +0200

    add methods

--- Bussiness layer/SensoreService.py
+++ Bussiness layer/SensoreService.py
@@ -1,4 +1,14 @@
 
 class SensoreService:
     def __init__(self, SensorController, RGB_Camera_Controller, Multispectral_Camera_Controller):
-        
+        self.SensorController = SensorController
+        self.RGB_Camera_Controller = RGB_Camera_Controller
+        self.Multispectral_Camera_Controller = Multispectral_Camera_Controller
+
+    def StartMesurement():
+        return 0
+    
+    def StopMesurement():
+        return 0
+
+    
